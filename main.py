a18435be0f36340aaa034402de509377b3d46cb7
Francois Boulogne <unpeudescience@free.fr>
2010-07-18 14:32:41 +0200

    clean up titles

--- main.py
+++ main.py
@@ -54,8 +54,15 @@ Reminder:
 
 
 def write_header(string,output):
+	title=""
+	if string.__class__ != str:
+		for i in string:
+			title+= i+" "
+        else:
+		title=string
+
 	header= '#############################################'
-	header= header + '\n' + '#   ' + str(string) + '\n'
+	header= header + '\n' + '#   ' + title + '\n'
 	header=header+'#############################################\n'
 	#print(header)
 	output.write(header)
