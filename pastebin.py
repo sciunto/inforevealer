e81bb108a16de40752e66d8bbb3048649382d323
Francois Boulogne <unpeudescience@free.fr>
2010-07-22 14:45:39 +0200

    add modification mention

--- pastebin.py
+++ pastebin.py
@@ -3,6 +3,9 @@
 #            Daniel Bartlett <dan@f-box.org>
 # Last modification : Mon Jan 28 22:33:23 CET 2008
 
+#Modified by Francois Boulogne <fboulogne at april dot org>
+#for inforevealer
+
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
