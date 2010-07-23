dd32606a6534a3999c2805c54ae276c4d8bb6825
Francois Boulogne <unpeudescience@free.fr>
2010-07-23 16:56:20 +0200

    clean

--- getinfo.py
+++ getinfo.py
@@ -42,7 +42,6 @@ class Command:
 			# correct OS or this info does not dependant on distrib ?
 			if self.linux_dependant == user_os or self.linux_dependant == None:
 				if(not os.getuid() == 0 and self.root):
-					#print("To get this, run the script as root")
 					output.write("To get this, run the script as root\n")
 				else:
 					proc = subprocess.Popen(self.command,stdout=subprocess.PIPE)
@@ -69,17 +68,13 @@ class File:
 			if self.linux_dependant == user_os or self.linux_dependant == None:
 				io.write_header(self.file,output)
 				if(not os.getuid() == 0 and self.root):
-					#print("To get this, run the script as root")
 					output.write("To get this, run the script as root\n")
 				else:
 					if os.path.isfile(self.file):
 						fhandler= open(self.file,'r')
-						t = fhandler.read()
-						#print(t)
-						output.write(t)
+						output.write( fhandler.read() )
 						fhandler.close()
 					else:
-						#print("The file "+str(self.file)+ " does not exist!")
 						output.write("The file "+str(self.file)+ " does not exist!")
 		else:
 			output.write('Use verbose option (-v) to print this file.')
@@ -98,11 +93,9 @@ def General_info(output):
 
 	mytime = time.gmtime()
 	output.write("date: "+ str(mytime[0])+"-"+str(mytime[1])+"-"+str(mytime[2])+" "+str(mytime[3])+ ":"+str(mytime[4])+"\n")
-	#print("date: "+ str(time[0])+"-"+str(time[1])+"-"+str(time[2])+" "+str(time[3])+ ":"+str(time[4]))
 
 	uname = subprocess.Popen(args=["uname","-a"],stdout=subprocess.PIPE).communicate()[0]
 	output.write("uname: "+str(uname)+"\n")
-	#print("uname: "+str(uname))
 
 
 
@@ -113,38 +106,32 @@ def General_info(output):
 		foo=fhandler.read()
 		fhandler.close()
 		output.write(foo)
-		#print(fhandler.read())
 	elif(os.path.isfile("/etc/SuSe-release")):
 		myos='suse'
 		fhandler=open("/etc/SuSe-release")
 		foo=fhandler.read()
 		fhandler.close()
 		output.write(foo)
-		#print(fhandler.read())
 	elif(os.path.isfile("/etc/mandriva-release")):
 		myos='mandriva'
 		fhandler=open("/etc/mandriva-release")
 		foo=fhandler.read()
 		fhandler.close()
 		output.write(foo)
-		#print(fhandler.read())
 	elif(os.path.isfile("/etc/redhat-release")):
 		myos='redhat'
 		fhandler=open("/etc/redhat-release")
 		foo=fhandler.read()
 		fhandler.close()
 		output.write(foo)
-		#print(fhandler.read())
 	elif(os.path.isfile("/etc/debian_version")):
 		myos='debian'
 		fhandler=open("/etc/debian_version")
 		foo=fhandler.read()
 		fhandler.close()
 		output.write(foo)
-		#print(fhandler.read())
 	else:
 		myos='unknown'
-		#print('Your distribution is unknown. Please, open a bug report with the command ls /etc.')
 		output.write('Your distribution is unknown. Please, open a bug report with the command ls /etc.')
 	return myos
 
